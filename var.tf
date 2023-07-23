9f65b9db8731ffb7f5838f6149c44455057f84b5
ak <ak@dummy.com>
2023-07-23 15:37:32 +0530

    first commit

---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "ec2_details" {
+    type = object({
+        ec2_ami = string
+        instance_name= string
+        key_pair_name=string
+    })
+    default ={
+        ec2_ami  = "ami-0376ec8eacdf70aae" 
+        instance_name= "t2.nano"
+        key_pair_name="shaheda_check234"
+    }
+}
+
